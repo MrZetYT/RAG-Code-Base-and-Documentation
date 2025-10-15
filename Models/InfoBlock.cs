cd51dc06abdd6f1fd8722e68ba23406821bc70a6
Mr_Zet <gesmannikita@gmail.com>
2025-10-15 23:01:33 +0300

    Add new migration, new model for InfoBlock

---
+++ Models/InfoBlock.cs
@@ -0,0 +1,17 @@
+﻿namespace RAG_Code_Base.Models
+{
+    public class InfoBlock
+    {
+        public Guid Id { get; set; }
+        public Guid FileItemId { get; set; }
+        public FileItem FileItem { get; set; }
+        public string Content { get; set; }
+        public string BlockType { get; set; }
+        public string? ClassName { get; set; }
+        public string? MethodName { get; set; }
+        public int StartLine { get; set; }
+        public int EndLine { get; set; }
+        public string? HeaderSection { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
